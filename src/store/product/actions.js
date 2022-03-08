25905a31001e62161cada7cf2ea688691c42dca6
denisa <73554758+denisademaku1@users.noreply.github.com>
2022-03-08 01:34:23 +0100

    D

--- src/store/product/actions.js
+++
@@ -1,8 +0,0 @@
-export function productDetails({ commit }, id) {
-    let url = "https://my-json-server.typicode.com/Nelzio/ecommerce-fake-json/products";
-    axios.get(url, { params: { id: id } }).then((response) => {
-        commit("setProduct", response.data[0]);
-    }).catch(function (error) {
-        console.log(error);
-    });
-}
