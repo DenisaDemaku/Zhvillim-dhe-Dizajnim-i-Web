44f7be785f4025226e7641ba8985bb9909864551
erasalihu <73554699+erasalihu@users.noreply.github.com>
2022-03-09 03:44:44 +0400

    Login.Register with Firebase

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,22 @@
 
 import About from './components/About';
 import Contact from './components/Contact';
+import Home from './components/Home';
+import Login from './components/auth/Login.vue'
+import Register from './components/auth/Register.vue'
+
+
+
 
 //importojme gjithe ato komponenta qe do i regjistrojme si routes
 
+
+
 export const routes = [
+    { path: '/', name:'home', component:Home},
     { path: '/about', name:'about', component:About},
-    { path: '/contact', name:'contact', component:Contact}
+    { path: '/contact', name:'contact', component:Contact},
+    { path: '/edit-user/:id', name:'EditUser', component:EditUser},
+    { path: '/login', name:'Login', component: Login},
+    { path: '/register', name:'Register', component: Register}
 ]
