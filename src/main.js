bd7a38c762e4aaa3fc1c5834d7734a3ff506b1c9
denisa <73554758+denisademaku1@users.noreply.github.com>
2022-03-08 02:24:35 +0100

    DB Connections

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
-createApp(App).mount('#app')
+// import { routes } from './routes';
+import router from './router'
+import store from './store'
+
+Vue.use(VueRouter);
+
+new Vue({
+  render: h => h(App),
+  store,
+  router
+}).$mount('#app')
