87b444979e2fb235d167616d7b48c5b4c3118bd7
denisa <73554758+denisademaku1@users.noreply.github.com>
2022-03-09 00:24:34 +0100

    Header, Footer, About P, Contact Page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
   <div id="app">
+    <app-Header></app-Header>
     <router-view></router-view>
+    <app-Footer></app-Footer>
   </div>
 </template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "App",
+  components: {
+    appHeader: Header,
+
+    appFooter: Footer,
+  },
+};
+</script>
 
 <style>
 @import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700";
